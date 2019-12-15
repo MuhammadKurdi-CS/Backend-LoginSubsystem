fd91e7c8ee476e35b561916223d01cfcc2cdeff3
Muhammad Mohammadi <moham732@coventry.ac.uk>
2019-12-15 20:19:46 +0000

    added the index file with relevant middleware for the project

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+//import koa dependencies
+var Koa = require('koa');
+var Router = require('koa-router');
+const cors = require('@koa/cors');
+const { userAgent } = require('koa-useragent'); 
+
+var app = new Koa();
+app.proxy=true //provides an account's Internet Protocol address
+
+var router = new Router();
+
+app.use(userAgent); 
+
+//import all the routes
+var account = require('./routes/account.js')
+var loginhistory = require('./routes/loginhistory.js')
+var admin = require('./routes/admin.js')
+
+//apply the routes as a middleware
+app.use(cors()); 
+app.use(account.routes());
+app.use(loginhistory.routes());
+app.use(admin.routes());
+
+//if there is no environment variable set for port number
+//use a default value of 3000
+var port = process.env.PORT || 3000; 
+
+app.listen(port); 
